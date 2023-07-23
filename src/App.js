fd66e0733ae58a8c47a76b57ff2d028c505bb64c
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-23 12:18:05 -0500

    importing data from data.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Main from "./Components/Main";
+import data from "./data";
 
 export default function App(){
+
     return(
        <div>
          <Navbar/>
