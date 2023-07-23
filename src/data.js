5815519702f083d77762376b6f7324686e45fb45
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-23 12:18:14 -0500

    adding data

--- src/data.js
+++ src/data.js
@@ -2,31 +2,31 @@ export default [
     {
         id:1,
         title:"Willis Tower",
-        location:"Chicago",
-        googleMapsUrl:"",
-        startDate:"",
-        endDate:"",
-        description:"",
-        imageUrl:""
+        location:"CHICAGO",
+        googleMapsUrl:"https://www.google.com/maps/place/Willis+Tower/@41.8788764,-87.6359149,15z/data=!4m6!3m5!1s0x880e2cbee269fbbb:0x10f6dce5eb88c7d!8m2!3d41.8788764!4d-87.6359149!16zL20vMDZfN2s?entry=ttu",
+        startDate:"07 Oct, 2018",
+        endDate:"09 Oct,  2018",
+        description:"Standing 110 stories tall, its black aluminum and bronze-tinted glass exterior has become emblematic of Chicago, a city crazy about its architecture. Although its record-breaking height has been surpassed several times over, its innovative structural design remains noteworthy. The Willis Tower laid the foundation for the supertall buildings being built today.",
+        imageUrl:"images/chicago.jpeg"
     },
     {
         id:2,
         title:"Time Square",
-        location:"NewYork",
-        googleMapsUrl:"",
-        startDate:"",
-        endDate:"",
-        description:"",
-        imageUrl:""
+        location:"NEWYORK",
+        googleMapsUrl:"https://www.google.com/maps/place/Times+Square/@40.7579747,-73.9855426,15z/data=!4m2!3m1!1s0x0:0x55194ec5a1ae072e?sa=X&ved=2ahUKEwjN7pXQqaWAAxXPmGoFHR8yAvAQ_BJ6BAhjEAA&ved=2ahUKEwjN7pXQqaWAAxXPmGoFHR8yAvAQ_BJ6BAh2EAg",
+        startDate:"13 Mar, 2019",
+        endDate:"15 Mar, 2019 ",
+        description:"Times Square is a major commercial intersection, tourist destination, entertainment hub, and neighborhood in Midtown Manhattan, New York City, United States. It is formed by the junction of Broadway, Seventh Avenue, and 42nd Street. Together with adjacent Duffy Square, Times Square is a bowtie-shaped plaza five blocks long between 42nd and 47th Streets",
+        imageUrl:"images/newyork.jpeg"
     },
     {
         id:3,
-        title:"Hatcher Pass",
-        location:"Alaska",
-        googleMapsUrl:"",
-        startDate:"",
-        endDate:"",
-        description:"",
-        imageUrl:""
+        title:"vail",
+        location:"COLORADO",
+        googleMapsUrl:"https://www.google.com/maps?output=search&q=vail+colorado&entry=mc&sa=X&ved=2ahUKEwjAg8yYqqWAAxXymmoFHQRJAQUQ0pQJegQIDhAB",
+        startDate:"29 DEC, 2021",
+        endDate:"31 DEC, 2021",
+        description:"North America's biggest and most popular ski resort, Vail is an alpine village in the heart of the Rocky Mountains. Long a favorite of serious skiers and jet setting celebrities, Vail now also draws a younger, hipper snowboarding crowd. Skiing is king at Vail Mountain and the Blue Sky Basin, but the area's vast splendor is perfect for hiking, biking, snowmobiling or scenery-gazing at any time of the year.",
+        imageUrl:"images/colorado.png"
     }
 ]
